c1bb7efd0aac699722e3371e4500b5a8fbaecdb6
Seekertimo <seekertimo@gmail.com>
2020-03-14 14:28:37 +0100

    Add files via upload

---
+++ Code/LogIn/Account/account.js
@@ -0,0 +1,8 @@
+
+function validate(){
+if ( document.getElementById('buttonH').clicked == true){
+    alert ("Logt in als patient Hestia");
+    window.location = "homepage.html"; // Naar andere pagina.
+    return false;
+    }
+}
